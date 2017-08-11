9cb75926b5e417a0ca912086060358b99baa79ef
Obrad Pesic <bato.pesic@gmail.com>
2017-08-11 15:56:44 +0200

    Added helper function to check if text exists in table row

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -38,7 +38,7 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly"
         # (Edith is very methodological)
@@ -51,8 +51,8 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list. Then she sees that the site has generated a unique URL
         # for her -- there is some explanatory text to that effect
@@ -62,6 +62,11 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL -- her to-do list is still there
 
         # Satisfied, she goes back to sleep
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
